33c0ed0e73bfc66f6c76ac0b4dea7116a326d5fa
@lucas.felipe <l17.lucas@gmail.com>
2021-08-19 12:11:45 -0300

    usando colections e tipos genericos

---
+++ Extensions/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_alura_List_Lambda_Linq.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void AddVarios<T>(this List<T> listadeinteiros, params T[] itens)
+        {
+            foreach (T item in itens)
+            {
+                listadeinteiros.Add(item);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Curso_alura_List_Lambda_Linq.Extensions;
+
 
 namespace Curso_alura_List_Lambda_Linq
 {
@@ -6,6 +9,23 @@ namespace Curso_alura_List_Lambda_Linq
     {
         static void Main(string[] args)
         {
+            List<int> a = new List<int>();
+            List<string> b = new List<string>();
+
+
+            a.Add(1);
+            a.Add(3);
+
+            a.AddVarios(1,3,6,5,4,7);
+
+            a.Remove(1);
+            a.Remove(6);
+            a.Remove(3);
+            a.Remove(1);
+            Console.WriteLine(a.Count);
+
+            b.AddVarios("asjdnjdas","fsdfsfds","doido");
+            Console.WriteLine(b.Count);
             
         }
     }
